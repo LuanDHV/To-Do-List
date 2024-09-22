91bd6755026995d6010a3f1e8aee7a16f52d3a09
LuanDHV <luandhv1406@gmail.com>
2024-09-22 16:53:33 +0700

    update

--- Frontend/src/component/TodoList.tsx
+++ Frontend/src/component/TodoList.tsx
@@ -10,19 +10,18 @@ const TodoList = () => {
     error: fetchError,
   } = useGetTodosQuery(undefined);
 
-  if (isFetching) return <p className="text-red-500">Loading</p>;
-  if (fetchError) return <p className="text-red-500">Error loading todos</p>;
-
   return (
     <div className="mx-auto mt-10 max-w-2xl">
       <h1 className="mb-5 text-center text-3xl font-bold">To-do List</h1>
       <AddTodo />
       <ul className="rounded-lg bg-white shadow">
-        {TodoList?.todos?.slice(0, 10).map((todo: TodoType) => (
+        {TodoList?.todos?.map((todo: TodoType) => (
           // Using ItemsTodo component to render each todo item
           <ItemsTodo key={todo.id} todo={todo} />
         ))}
       </ul>
+      {fetchError && <p className="text-red-500">Failed to add todo</p>}
+      {isFetching && <p className="text-red-500">Loading</p>}
     </div>
   );
 };
